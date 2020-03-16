af7d21c8d075abddc72bdbf454fb8f735ad90b82
Markus Peura <Markus.Peura@iki.fi>
2020-03-16 15:23:46 +0200

    Major

--- rack/main/rack.h
+++ rack/main/rack.h
@@ -32,7 +32,7 @@ Neighbourhood Partnership Instrument, Baltic Sea Region Programme 2007-2013)
 #ifndef __RACK_API__
 #define __RACK_API__
 #define __RACK__ "Rack_fmi.fi"
-#define __RACK_VERSION__ "5.21"
+#define __RACK_VERSION__ "5.25"
 
 /**
  *   Version 5.08
