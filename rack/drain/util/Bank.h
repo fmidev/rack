a28404c224783c52105433b938df12f441261f1b
Markus Peura <Markus.Peura@iki.fi>
2020-09-27 19:01:46 +0300

    Ptr design

--- rack/drain/util/Bank.h
+++ rack/drain/util/Bank.h
@@ -43,6 +43,262 @@ Neighbourhood Partnership Instrument, Baltic Sea Region Programme 2007-2013)
 namespace drain
 {
 
+
+/**
+ *  \tparam T - value type (base type for cloning)
+ *  \tparam K - key type
+ */
+template <class T, class K=std::string>
+class Bank2 : protected std::map<std::string, ClonerBase<T> *> {
+
+public:
+
+	//typedef std::string str_t;
+
+	/// Public key type. (Key type used in the public interface.)
+	typedef K key_t;
+
+	typedef ClonerBase<T> cloner_t;
+
+	typedef std::map<std::string, cloner_t *> map_t;
+
+	/// Adds class D as an internal instance.
+	/**
+	 *  For each class D, a single static instance is created.
+	 *
+	 *  It is allowed to adding a class repeatedly with different keys.
+	 *
+	 *  Changing static to external instance and vice versa causes a warning.
+	 */
+	template <class D>
+	D & add(const K & key){
+		//const std::string & k = ;
+		static drain::Cloner<T,D> cloner;
+		set(resolve(key), cloner);
+		return cloner.src;
+	}
+
+	/// Adds class D as an external instance.
+	/**
+	 *  For each class D, a single static instance is created.
+	 *
+	 *  It is allowed to adding a class repeatedly with different keys.
+	 *
+	 *  Changing static to external instance and vice versa causes a warning.
+	 *  Changing external instances is allowed.
+	 */
+	template <class D>
+	D & addExternal(const K & key, D & entry){
+		static drain::Cloner<T,D> cloner(entry);
+		set(resolve(key), cloner);
+		return cloner.src;
+	}
+
+	/// Check if a cloner is defined for this key.
+	inline
+	bool has(const K & key) const {
+		return (this->find(resolve(key)) != this->end());
+	}
+
+	/// Return a copy of the base instance.
+	inline
+	T & clone(const K & key) const {
+		typename map_t::const_iterator it = this->find(resolve(key));
+		if (it != this->end()){
+			return it->second->clone();
+		}
+		else {
+			throw std::runtime_error(resolve(key) + ": no such entry");
+		}
+	}
+
+	/// Returns the base instance.
+	inline
+	T & get(const K & key){
+		typename map_t::iterator it = this->find(resolve(key));
+		if (it != this->end()){
+			return it->second->get();
+		}
+		else {
+			throw std::runtime_error(resolve(key) + ": no such entry");
+		}
+	}
+
+	/// Return the internal static entry.
+	/*
+	 *  This method is useful if one wants to first set parameters of the
+	 *  source and the create clones of it.
+	 *
+	 */
+	inline
+	cloner_t & getCloner(const K & key) const {
+		typename map_t::const_iterator it = this->find(resolve(key));
+		if (it != this->end()){
+			return *(it->second);
+		}
+		else {
+			throw std::runtime_error(key + ": no such entry");
+		}
+	}
+
+	/*
+	inline
+	void clear(const K &key, T *ptr){
+		cloner_t & cloner = getCloner(key);
+		cloner.clear(*ptr);
+	}
+	*/
+
+	///
+	inline
+	const map_t & getMap() const{
+		return *this;
+	}
+
+	///
+	/**
+	 *  If K != std::string, this class must be derived and this function redefined.
+	 */
+	virtual
+	const std::string & resolve(const K & key) const {
+		return key;
+	}
+
+
+protected:
+
+	/**
+	 *   Raises a warning if cloner is changed from external to internal, or vice versa.
+	 */
+	drain::ClonerBase<T> & set(const std::string & key, drain::ClonerBase<T> & cloner){
+
+		const typename map_t::iterator it = this->find(key);
+
+		if (it != this->end()){
+			// Redefined existing (making alias?)
+			if (it->second != &cloner){
+				std::cerr << __FILE__ << " warning: changing cloner source for: " << key << "\n";
+				//throw std::runtime_error(key + ": no such entry");
+			}
+			else {
+				// Re-introduce existing?
+				std::cerr << __FILE__ << " warning: re-defining: " << key << "\n";
+			}
+			it->second = &cloner;
+		}
+		else {
+			// Create new
+			this->operator [](key) = &cloner;
+		}
+
+		return cloner;
+
+	}
+
+};
+
+// WIth alias support
+template <class T>
+class BankSuper : public Bank2<T, std::string> {
+
+public:
+
+	typedef Bank2<T, std::string> bank_t;
+	typedef typename bank_t::key_t key_t;
+
+	// Overrides
+
+	template <class D>
+	D & add(const std::string & key, char alias=0){
+		if (alias)
+			setAlias(alias, key);
+		return Bank2<T, std::string>::template add<D>(key);
+	}
+
+	template <class D>
+	D & addExternal(const std::string & key, D & entry){
+		return Bank2<T, std::string>::template addExternal<D>(key, entry);
+	}
+
+	template <class D>
+	D & addExternal(const std::string & key, char alias, D & entry){
+		if (alias)
+			setAlias(alias, key);
+		return Bank2<T, std::string>::template addExternal<D>(key, entry);
+	}
+
+
+
+
+	inline
+	void setAlias(char alias, const std::string & value){
+		aliases.add(alias, value);
+	}
+
+	inline
+	bool hasAlias(const std::string & value) const {
+		if (value.empty())
+			return false;
+		else if (value.length()==1)
+			return hasAlias(value.at(0));
+		else
+			return aliases.hasValue(value);
+	}
+
+	inline
+	bool hasAlias(char alias) const {
+		return aliases.hasKey(alias);
+	}
+
+	inline
+	char getAlias(const std::string & value) const {
+		if (value.length() > 1)
+			return aliases.getKey(value);
+		else if (value.length()==1){
+			// Warn ?
+			char c = value.at(0);
+			if (aliases.hasKey(c))
+				return c;
+		}
+		return '\0';
+	}
+
+
+
+	inline
+	const drain::Dictionary2<char, key_t> & getAliases() const {
+		return aliases;
+	}
+
+
+	/// Given alias or long key, returns the long key .
+	/**
+	 *
+	 */
+	virtual inline
+	const std::string & resolve(const key_t & value) const {
+		//std::cout << __FUNCTION__ << ':' << value << '\n';
+		if (value.length() == 1)
+			return aliases.getValue(value.at(0));
+		else
+			return value;
+	}
+
+	void toStream(std::ostream & ostr = std::cout) const {
+		for (typename bank_t::const_iterator it = this->begin(); it != this->end(); ++it) {
+			ostr << it->first << ':' << it->second->count() << '\n';
+		}
+	}
+
+
+protected:
+
+	drain::Dictionary2<char, std::string> aliases;
+
+
+};
+
+
 /// A registry that contains items that can be cloned with clone() or referenced directly with get().
 template <class T>
 class Bank : public Registry<ClonerBase<T> > {
@@ -54,8 +310,6 @@ public:
 	void add2(const std::string & key){
 		static drain::Cloner<T,D> cloner;
 		this->add(cloner, key);
-		// x.get().value = 12345;
-		//std::cout << " Atte " << this->get(key).get().value << '\n';
 	}
 
 	inline
