05a2a4ac831b8731844081587ee30dd0742c09d4
Markus Peura <markus.peura@fmi.fi>
2024-06-12 13:23:47 +0300

    v8.9.1

--- src/main/rack.h
+++ src/main/rack.h
@@ -32,7 +32,7 @@ Neighbourhood Partnership Instrument, Baltic Sea Region Programme 2007-2013)
 #ifndef __RACK_API__
 #define __RACK_API__
 #define __RACK__ "Rack_fmi.fi"
-#define __RACK_VERSION__ "8.9"
+#define __RACK_VERSION__ "8.9.1"
 
 /**
  *   Version 8.9.1
