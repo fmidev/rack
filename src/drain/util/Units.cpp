6104f4fba9604a97e15fb73b2d9f4cb30e5b087a
Markus Peura <markus.peura@fmi.fi>
2026-03-19 10:51:22 +0200

    v19.1.7 Units.cpp

---
+++ src/drain/util/Units.cpp
@@ -0,0 +1,61 @@
+/*
+
+MIT License
+
+Copyright (c) 2017 FMI Open Development / Markus Peura, first.last@fmi.fi
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+*/
+/*
+Part of Rack development has been done in the BALTRAD projects part-financed
+by the European Union (European Regional Development Fund and European
+Neighbourhood Partnership Instrument, Baltic Sea Region Programme 2007-2013)
+*/
+
+#include "Units.h"
+
+namespace drain {
+
+DRAIN_ENUM_DICT(Unit) = {
+		//{"", Unit::UNDEFINED},
+		{"deg", Unit::DEGREE},
+		{"rad", Unit::RADIAN},
+		{"m",   Unit::METRE},
+		{"px",  Unit::PIXEL}, // CSS notation
+};
+
+/*
+Unit Units::extract(const std::string & expr){
+
+	const size_t N = expr.length();
+	for (const auto & entry: Enum<Unit>::dict){
+		if (expr.find(entry.first, N-entry.first.length())){
+			return entry.second;
+		}
+	}
+
+	return UNDEFINED;
+}
+*/
+
+
+} // ::drain
+
+
