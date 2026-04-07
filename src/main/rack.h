10230e8cb4c7f2b9e38e987e8008de20a9d28692
Markus Peura <markus.peura@fmi.fi>
2026-04-07 13:24:19 +0300

    v19.2.0

--- src/main/rack.h
+++ src/main/rack.h
@@ -33,7 +33,7 @@ Neighbourhood Partnership Instrument, Baltic Sea Region Programme 2007-2013)
 #define __RACK_API__
 
 #include <drain/Version.h>
-#define RACK_VERSION drain::Version<>(19,2)
+#define RACK_VERSION drain::Version<>(19,2,0)
 #define RACK_STAMP "Rack_fmi.fi"
 
 
@@ -71,6 +71,8 @@ Neighbourhood Partnership Instrument, Baltic Sea Region Programme 2007-2013)
  *  - gRadarLabel, final version
  *  - gPanel adjusted
  *  - CTEXT VERT alignment fixed
+ *  Version 19.2.0
+ *  - Python3 (.py) edits only
  *
  *  Version 18
  *  - SVG basic command set completed
